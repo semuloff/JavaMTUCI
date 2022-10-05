606284dcc794257f6f0fb43eb95a529c1088ce3b
SEMUL <semul.offical@icloud.com>
2022-10-06 01:00:45 +0300

    first string Java output program

---
+++ com/lessons/Lesson_1.java
@@ -0,0 +1,8 @@
+package com.lessons;
+
+// first simple Java output program "Bye, World!"
+public class Lesson_1 {
+    public static void main(String[] args) {
+        System.out.println("Bye, World!");
+    }
+}
