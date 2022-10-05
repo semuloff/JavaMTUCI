d5cd9cf9ffad2b14764f003a425727088733805a
SEMUL <semul.offical@icloud.com>
2022-10-06 01:29:19 +0300

    the first simple 'Bye, World' console output program in Java

---
+++ com/lessons/Lesson_1.java
@@ -0,0 +1,8 @@
+package com.lessons;
+
+// the first simple "Bye World" console output program in Java
+public class Lesson_1 {
+    public static void main(String[] args) {
+        System.out.println("Bye, World!");
+    }
+}
