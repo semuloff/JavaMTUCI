b6b3b36451d5e417addc4f3596f6d056ad03143e
SEMUL <semul.offical@icloud.com>
2022-10-10 15:55:32 +0300

    init task_2

---
+++ Tasks/task_2/src/Task_2.java
@@ -0,0 +1,12 @@
+public class Task_2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // PrettyPrint
+    public static void prettyPrint(String task, String functionName) {
+        System.out.println("\n~~~~~~ " + task + ": " + functionName + " ~~~~~~");
+    }
+}
+
