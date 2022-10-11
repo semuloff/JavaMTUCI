def28502fb70fb0fb333211fac895b8dea47fc20
SEMUL <semul.offical@icloud.com>
2022-10-11 12:48:52 +0300

    task (1/10) is done!

--- Tasks/task_2/src/Task_2.java
+++ Tasks/task_2/src/Task_2.java
@@ -1,12 +1,36 @@
 public class Task_2 {
 
     public static void main(String[] args) {
+        // (1/10)
+        prettyPrint("(1/10)", "repeat");
+        System.out.println(repeat("mice", 5)); // ("mice", 5) -> "mmmmmiiiiiccccceeeee"
+        System.out.println(repeat("hello", 3)); // ("hello", 3) ➞ "hhheeellllllooo"
+        System.out.println(repeat("stop", 1)); // ("stop", 1) ➞ "stop"
 
+        // (2/10)
+        // prettyPrint("(1/10)", "differenceMaxMin");
+        // System.out.println(differenceMaxMin());
     }
 
     // PrettyPrint
     public static void prettyPrint(String task, String functionName) {
         System.out.println("\n~~~~~~ " + task + ": " + functionName + " ~~~~~~");
     }
+
+    // (1/10)
+    public static String repeat(String arg, int conut) {
+        String answer = "";
+
+        for (int i = 0; i < arg.length(); i++) {
+            answer += (arg.charAt(i) + "").repeat(conut);
+        }
+
+        return answer;
+    }
+
+    // (2/10)
+    // public static void differenceMaxMin(int[] args) {
+    //    int maxNumber, minNumber;
+    //}
 }
 
