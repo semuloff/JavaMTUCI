4cc51c5c7698e053f4d2e4a0cde9bae19fb8db28
SEMUL <semul.offical@icloud.com>
2022-10-20 00:10:11 +0300

    studied the concept of 'methods'

--- com/lessons/Lesson_8.java
+++ com/lessons/Lesson_8.java
@@ -1,6 +1,15 @@
-
 public class Lesson_8 {
     public static void main(String[] args) {
+        // functions
+        byeWorld("Mark");
+        System.out.println(sum(4, 3));
+    }
 
+    public static void byeWorld(String item) {
+        System.out.println("Bye, World and " + item + "!");
+    }
+
+    public static int sum(int firstNumb, int secondNumb) {
+        return firstNumb + secondNumb;
     }
 }
