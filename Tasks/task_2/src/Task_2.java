ff02f870969d23321b7e74b67c201a7e791a7178
SEMUL <semul.offical@icloud.com>
2022-10-22 12:38:33 +0300

    tasks (10/10) done

--- Tasks/task_2/src/Task_2.java
+++ Tasks/task_2/src/Task_2.java
@@ -34,12 +34,35 @@ public class Task_2 {
 
         // (6/10)
         prettyPrint("(6/10)", "Fibonacci");
-        System.out.println(Fibonacci(3)); // (3) -> 3
+        System.out.println(Fibonacci(3)); // (3) -> 2
         System.out.println(Fibonacci(7)); // (3) -> 13
         System.out.println(Fibonacci(12)); // (3) -> 144
 
         // (7/10)
         prettyPrint("(7/10)", "isValid");
+        System.out.println(isValid("59001")); // "59001" -> true
+        System.out.println(isValid("853a7")); // "853a7" -> false
+        System.out.println(isValid("732 32")); // "732 32" -> false
+        System.out.println(isValid("393939")); // "393939" -> false
+
+        // (8/10)
+        prettyPrint("(8/10)", "isStrangePair");
+        System.out.println(isStrangePair("sparkling", "groups")); // ("sparkling", "groups") ➞ true
+        System.out.println(isStrangePair("bush", "hubris")); // ("bush", "hubris") ➞ false
+        System.out.println(isStrangePair("", "")); // ("", "") ➞ true
+
+        // (9/10)
+        prettyPrint("(9/10)", "isPrefix && isSuffix");
+        System.out.println(isPrefix("automation", "auto-")); // ("automation", "auto-") -> true
+        System.out.println(isSuffix("arachnophobia", "-phobia")); // ("arachnophobia", "-phobia") -> true
+        System.out.println(isPrefix("retrospect", "sub-")); // ("retrospect", "sub-") -> false
+        System.out.println(isSuffix("vocation", "-logy")); // ("vocation", "-logy") -> false
+
+        // (10/10)
+        prettyPrint("(10/10)", "boxSeq");
+        System.out.println(boxSeq(0)); // (0) -> 0
+        System.out.println(boxSeq(1)); // (1) -> 3
+        System.out.println(boxSeq(2)); // (2) -> 2
     }
 
     // PrettyPrint
@@ -58,7 +81,7 @@ public class Task_2 {
         return answer;
     }
 
-     //(2/10)
+    //(2/10)
     public static int differenceMaxMin(int[] arr) {
         int maxNumber = arr[0];
         int minNumber = maxNumber;
@@ -121,17 +144,81 @@ public class Task_2 {
     }
 
     // (6/10)
-    public static int Fibonacci(int n) {
-        switch (n) {
-            case 0:
-                return 0;
-            case 1:
-                return 1;
-            default:
-                return Fibonacci(n - 1) + Fibonacci(n - 2);
+    public static long Fibonacci(int n) {
+        long[] fibN = new long[n + 1];
+        fibN[0] = 0;
+        fibN[1] = 1;
+
+        for (int index = 2; index <= n; index++) {
+            fibN[index] = fibN[index - 1] + fibN[index - 2];
         }
+
+        return fibN[n];
     }
 
     // (7/10)
+    public static boolean isValid(String input) {
+        if (input.length() <= 5) {
+            char symbol;
+
+            for (int index = 0; index < input.length(); index++) {
+                symbol = input.charAt(index);
+                if (symbol == ' ' || !Character.isDigit(symbol))
+                    return false;
+            }
+
+            return true;
+        } else
+            return false;
+    }
+
+    // (8/10)
+    public static boolean isStrangePair(String strOne, String strTwo) {
+        if (strOne.length() == 0 && strTwo.length() == 0) {
+            return true;
+        }
+
+        return strOne.charAt(0) == strTwo.charAt(strTwo.length() - 1)
+                && strTwo.charAt(0) == strOne.charAt(strOne.length() - 1);
+    }
+
+    // (9/10)
+    public static boolean isPrefix(String word, String prefix) {
+        for (int index = 0; index < prefix.length() - 1; index++) {
+            if (word.charAt(index) != prefix.charAt(index))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isSuffix(String word, String suffix) {
+        final int transition = word.length() - (suffix.length() - 1);
+
+        for (int indexW = transition; indexW < word.length(); indexW++) {
+            if (word.charAt(indexW) != suffix.charAt(indexW - transition + 1))
+                return false;
+        }
+
+        return true;
+    }
+
+    // (10/10)
+    public static int boxSeq(int n) {
+        int count = 0;
+
+        for (int step = 0; step <= n; step++) {
+            if (step % 2 == 0) {
+                if (step == 0)
+                    count = 0;
+                else
+                    count -= 1;
+            } else
+                count += 3;
+
+        }
+
+        return count;
+    }
 }
 
