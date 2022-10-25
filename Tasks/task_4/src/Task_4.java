3475dbb32a66e8bed7fe6518038d24df28b20e7f
SEMUL <semul.offical@icloud.com>
2022-10-26 01:24:47 +0300

    init task_4

---
+++ Tasks/task_4/src/Task_4.java
@@ -0,0 +1,12 @@
+public class Task_4 {
+
+    public static void main(String[] args) {
+        // (1/10)
+        prettyPrint("(1/10)", "");
+    }
+    // PrettyPrint
+    public static void prettyPrint(String task, String functionName) {
+        System.out.println("\n~~~~~~ " + task + ": " + functionName + " ~~~~~~");
+    }
+}
+
