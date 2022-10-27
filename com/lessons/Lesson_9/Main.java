681f2b34b940f2b1a701ed11bdce0a4e07fb2fc9
SEMUL <semul.offical@icloud.com>
2022-10-27 16:08:37 +0300

    code refactoring

--- com/lessons/Lesson_9/Main.java
+++ com/lessons/Lesson_9/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // basics of OOP.
         Transport tayota = new Transport();
-        tayota.setW(234.34f, 2340, "black", new byte[] {0, 0, 0});
 
+        // methods in use.
+        tayota.setW(234.34f, 2340, "black", new byte[] {0, 0, 0});
         tayota.printOptions();
         tayota.moveX(20);
         tayota.moveZ(23);
         tayota.printOptions();
 
         Transport bmv = new Transport();
-        bmv.setW(235.3f, 2430, "White", new byte[] {1, 3, 64});
 
+        bmv.setW(235.3f, 2430, "White", new byte[] {1, 3, 64});
         bmv.printOptions();
     }
-
 }
