68fdc32d4ccd7aac74955aa5de3363fd8c35ed47
SEMUL <semul.offical@icloud.com>
2022-10-27 16:18:55 +0300

    added comment

--- com/lessons/Lesson_9/Main.java
+++ com/lessons/Lesson_9/Main.java
@@ -3,6 +3,7 @@ package com.lessons.Lesson_9;
 public class Main {
     public static void main(String[] args) {
         // basics of OOP.
+        // class instance.
         Transport tayota = new Transport();
 
         // methods in use.
@@ -12,6 +13,7 @@ public class Main {
         tayota.moveZ(23);
         tayota.printOptions();
 
+        // class instance.
         Transport bmv = new Transport();
 
         bmv.setW(235.3f, 2430, "White", new byte[] {1, 3, 64});
