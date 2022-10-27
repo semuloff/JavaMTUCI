f671abedd155cda70f858a3d066c6e34e5255009
SEMUL <semul.offical@icloud.com>
2022-10-27 16:22:35 +0300

    refactoring lessons

--- com/lessons/Lesson_1.java
+++
@@ -1,6 +0,0 @@
-// the first simple "Bye World" console output program in Java
-public class Lesson_1 {
-    public static void main(String[] args) {
-        System.out.println("Bye, World!");
-    }
-}
