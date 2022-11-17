ce1caaca2b42f2b5cb2d25b7c65a68d4f99bd780
SEMUL <semul.main@icloud.com>
2022-11-17 03:04:52 +0300

    lab 4

---
+++ Labs/Lab_4/src/JImageDisplay.java
@@ -0,0 +1,37 @@
+import javax.swing.JComponent;
+import java.awt.image.BufferedImage;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+
+public class JImageDisplay extends JComponent {
+    // Управляет изображением, содержимое которого можно записать.
+    private BufferedImage image;
+
+    public JImageDisplay(int width, int height) {
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Dimension dim = new Dimension(width, height);
+
+        super.setPreferredSize(dim);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+    }
+
+    public void clearImage() {
+        // Заменяем каждый пиксель на черный.
+        for (int width = 0; width < image.getWidth(); width++) {
+            for (int height = 0; height < image.getHeight(); height++) {
+                // Делает [width][height] пиксель черным.
+                image.setRGB(width, height, 0);
+            }
+        }
+    }
+
+    public void drawPixel(int x, int y, int rgbColor) {
+        // Устанавливаем определенный цвет для пикселя.
+        image.setRGB(x, y, rgbColor);
+    }
+}
