e3aa8549d618b6bc333a2542446ee775d666c7ee
SEMUL <semul.main@icloud.com>
2022-12-14 18:01:35 +0300

    (7/10)

--- Tasks/task_5/src/Task_5.java
+++ Tasks/task_5/src/Task_5.java
@@ -7,43 +7,50 @@ public class Task_5 {
 
     public static void main(String[] args) {
         // (1/10)
-        prettyPrint("(1/10)", "encrypt & decrypt");
-        System.out.println(encrypt("Hello")); // ("Hello") -> [72, 29, 7, 0, 3]
-        System.out.println(decrypt(new int[]{72, 33, -73, 84, -12, -3, 13, -13, -68})); // [ 72, 33, -73, 84, -12, -3, 13, -13, -68 ]) -> "Hi there!"
-        System.out.println(encrypt("Sunshine")); // "Sunshine" -> [83, 34, -7, 5, -11, 1, 5, -9]
-
-        // (2/10)
-        prettyPrint("(2/10)", "canMove");
-        System.out.println(canMove("Rook", "A8", "H8")); // ("Rook", "A8", "H8") -> true
-        System.out.println(canMove("Bishop", "A7", "G1")); // ("Bishop", "A7", "G1") -> true
-        System.out.println(canMove("Queen", "C4", "D6")); // ("Queen", "C4", "D6") -> false
-
-        // (3/10)
-        prettyPrint("(3/10)", "canComplete");
-        System.out.println(canComplete("butl", "beautiful")); // canComplete("butl", "beautiful") -> true
-        System.out.println(canComplete("butlz", "beautiful")); // canComplete("butlz", "beautiful") -> false
-        System.out.println(canComplete("tulb", "beautiful")); // canComplete("tulb", "beautiful") -> false
-        System.out.println(canComplete("bbutl", "beautiful")); // canComplete("bbutl", "beautiful") -> false
-
-        // (4/10)
-        prettyPrint("(4/10)", "sumDigProd");
-        System.out.println(sumDigProd(16, 28)); // sumDigProd(16, 28) -> 6
-        System.out.println(sumDigProd(0)); // sumDigProd(0) -> 0
-        System.out.println(sumDigProd(1, 2, 3, 4, 5, 6)); // sumDigProd(1, 2, 3, 4, 5, 6) -> 2
-
-        // (5/10)
-        prettyPrint("(5/10)", "sameVowelGroup");
-        System.out.println(sameVowelGroup(new String[] {"toe", "ocelot", "maniac"})); // ["toe", "ocelot", "maniac"] -> ["toe", "ocelot"]
-        System.out.println(sameVowelGroup(new String[] {"many", "carriage", "emit", "apricot", "animal"})); // ["many", "carriage", "emit", "apricot", "animal"] -> ["many"]
-        System.out.println(sameVowelGroup(new String[] {"hoops", "chuff", "bot", "bottom"})); // ["hoops", "chuff", "bot", "bottom"] -> ["hoops", "bot", "bottom"]
-
-        // (6/10)
-        prettyPrint("(6/10)", "validateCard");
-        System.out.println(validateCard(1234567890123456L)); // validateCard(1234567890123456) -> false
-        System.out.println(validateCard(1234567890123452L)); // validateCard(1234567890123452) -> true
+//        prettyPrint("(1/10)", "encrypt & decrypt");
+//        System.out.println(encrypt("Hello")); // ("Hello") -> [72, 29, 7, 0, 3]
+//        System.out.println(decrypt(new int[]{72, 33, -73, 84, -12, -3, 13, -13, -68})); // [ 72, 33, -73, 84, -12, -3, 13, -13, -68 ]) -> "Hi there!"
+//        System.out.println(encrypt("Sunshine")); // "Sunshine" -> [83, 34, -7, 5, -11, 1, 5, -9]
+//
+//        // (2/10)
+//        prettyPrint("(2/10)", "canMove");
+//        System.out.println(canMove("Rook", "A8", "H8")); // ("Rook", "A8", "H8") -> true
+//        System.out.println(canMove("Bishop", "A7", "G1")); // ("Bishop", "A7", "G1") -> true
+//        System.out.println(canMove("Queen", "C4", "D6")); // ("Queen", "C4", "D6") -> false
+//
+//        // (3/10)
+//        prettyPrint("(3/10)", "canComplete");
+//        System.out.println(canComplete("butl", "beautiful")); // canComplete("butl", "beautiful") -> true
+//        System.out.println(canComplete("butlz", "beautiful")); // canComplete("butlz", "beautiful") -> false
+//        System.out.println(canComplete("tulb", "beautiful")); // canComplete("tulb", "beautiful") -> false
+//        System.out.println(canComplete("bbutl", "beautiful")); // canComplete("bbutl", "beautiful") -> false
+//
+//        // (4/10)
+//        prettyPrint("(4/10)", "sumDigProd");
+//        System.out.println(sumDigProd(16, 28)); // sumDigProd(16, 28) -> 6
+//        System.out.println(sumDigProd(0)); // sumDigProd(0) -> 0
+//        System.out.println(sumDigProd(1, 2, 3, 4, 5, 6)); // sumDigProd(1, 2, 3, 4, 5, 6) -> 2
+//
+//        // (5/10)
+//        prettyPrint("(5/10)", "sameVowelGroup");
+//        System.out.println(sameVowelGroup(new String[] {"toe", "ocelot", "maniac"})); // ["toe", "ocelot", "maniac"] -> ["toe", "ocelot"]
+//        System.out.println(sameVowelGroup(new String[] {"many", "carriage", "emit", "apricot", "animal"})); // ["many", "carriage", "emit", "apricot", "animal"] -> ["many"]
+//        System.out.println(sameVowelGroup(new String[] {"hoops", "chuff", "bot", "bottom"})); // ["hoops", "chuff", "bot", "bottom"] -> ["hoops", "bot", "bottom"]
+//
+//        // (6/10)
+//        prettyPrint("(6/10)", "validateCard");
+//        System.out.println(validateCard(1234567890123456L)); // validateCard(1234567890123456) -> false
+//        System.out.println(validateCard(1234567890123452L)); // validateCard(1234567890123452) -> true
 
         // (7/10)
-        prettyPrint("(7/10)", "numToEng");
+        prettyPrint("(7/10)", "numToEng & numToRus");
+        System.out.println(numToEng(0)); // numToEng(0) -> "zero"
+        System.out.println(numToEng(18)); // numToEng(18) -> "eighteen"
+        System.out.println(numToEng(126)); // numToEng(126) -> "one hundred twenty six"
+        System.out.println(numToEng(909)); // numToEng(909) -> "nine hundred nine"
+
+        // (8/10)
+        prettyPrint("(8/10)", "getSha256Hash");
     }
     
     // PrettyPrint
@@ -268,7 +275,302 @@ public class Task_5 {
     }
 
     // (7/10)
-    public static void numToEng(short number) {
+    public static String numToEng(int number) {
+            if (number == 0) {
+                return "zero";
+            }
+
+            StringBuilder build = new StringBuilder();
+
+            if (number / 100 > 0) {
+                switch (number / 100) {
+                    case 1:
+                        build.append("one hundred");
+                        break;
+                    case 2:
+                        build.append("two hundred");
+                        break;
+                    case 3:
+                        build.append("three hundred");
+                        break;
+                    case 4:
+                        build.append("four hundred");
+                        break;
+                    case 5:
+                        build.append("fife hundred");
+                        break;
+                    case 6:
+                        build.append("six hundred");
+                        break;
+                    case 7:
+                        build.append("seven hundred");
+                        break;
+                    case 8:
+                        build.append("eight hundred");
+                        break;
+                    case 9:
+                        build.append("nine hundred");
+                        break;
+                }
+
+                number %= 100;
+
+                if (number > 0) {
+                    build.append(" ");
+                }
+            }
+
+            if (number / 10 > 0 && number % 10 >= 0) {
+                if (number / 10 == 1) {
+                    switch (number % 10) {
+                        case 0:
+                            build.append("ten");
+                            break;
+                        case 1:
+                            build.append("eleven");
+                            break;
+                        case 2:
+                            build.append("twelve");
+                            break;
+                        case 3:
+                            build.append("thirteen");
+                            break;
+                        case 4:
+                            build.append("fourteen");
+                            break;
+                        case 5:
+                            build.append("fifteen");
+                            break;
+                        case 6:
+                            build.append("sixteen");
+                            break;
+                        case 7:
+                            build.append("seventeen");
+                            break;
+                        case 8:
+                            build.append("eighteen");
+                            break;
+                        case 9:
+                            build.append("nineteen");
+                            break;
+                    }
+
+                    number = 0;
+                } else {
+                    switch (number / 10) {
+                        case 2:
+                            build.append("twenty");
+                            break;
+                        case 3:
+                            build.append("thirty");
+                            break;
+                        case 4:
+                            build.append("fourty");
+                            break;
+                        case 5:
+                            build.append("fifty");
+                            break;
+                        case 6:
+                            build.append("sixty");
+                            break;
+                        case 7:
+                            build.append("seventy");
+                            break;
+                        case 8:
+                            build.append("eighty");
+                            break;
+                        case 9:
+                            build.append("ninety");
+                            break;
+                    }
+
+                    build.append(" ");
+                    number %= 10;
+                }
+            }
+
+            switch (number) {
+                case 1:
+                    build.append("one");
+                    break;
+                case 2:
+                    build.append("two");
+                    break;
+                case 3:
+                    build.append("three");
+                    break;
+                case 4:
+                    build.append("four");
+                    break;
+                case 5:
+                    build.append("fife");
+                    break;
+                case 6:
+                    build.append("six");
+                    break;
+                case 7:
+                    build.append("seven");
+                    break;
+                case 8:
+                    build.append("eight");
+                    break;
+                case 9:
+                    build.append("nine");
+                    break;
+            }
+
+            return build.toString();
+    }
+
+    public static String numToRus(int number) {
+        if (number == 0) {
+            return "ноль";
+        }
+
+        StringBuilder build = new StringBuilder();
+
+        if (number / 100 > 0) {
+            switch (number / 100) {
+                case 1:
+                    build.append("сто");
+                    break;
+                case 2:
+                    build.append("двести");
+                    break;
+                case 3:
+                    build.append("триста");
+                    break;
+                case 4:
+                    build.append("четыреста");
+                    break;
+                case 5:
+                    build.append("пятьсот");
+                    break;
+                case 6:
+                    build.append("шестьсот");
+                    break;
+                case 7:
+                    build.append("семьсот");
+                    break;
+                case 8:
+                    build.append("восемьсот");
+                    break;
+                case 9:
+                    build.append("девятьсот");
+                    break;
+            }
+
+            number %= 100;
+
+            if (number > 0) {
+                build.append(" ");
+            }
+        }
+
+        if (number / 10 > 0 && number % 10 >= 0) {
+            if (number / 10 == 1) {
+                switch (number % 10) {
+                    case 0:
+                        build.append("десять");
+                        break;
+                    case 1:
+                        build.append("одиннадцать");
+                        break;
+                    case 2:
+                        build.append("двенадцать");
+                        break;
+                    case 3:
+                        build.append("тринадцать");
+                        break;
+                    case 4:
+                        build.append("четырнадцать");
+                        break;
+                    case 5:
+                        build.append("пятнадцать");
+                        break;
+                    case 6:
+                        build.append("шестнадцать");
+                        break;
+                    case 7:
+                        build.append("семнадцать");
+                        break;
+                    case 8:
+                        build.append("восемнадцать");
+                        break;
+                    case 9:
+                        build.append("девятнадцать");
+                        break;
+                }
+
+                number = 0;
+            } else {
+                switch (number / 10) {
+                    case 2:
+                        build.append("двадцать");
+                        break;
+                    case 3:
+                        build.append("тридцать");
+                        break;
+                    case 4:
+                        build.append("сорок");
+                        break;
+                    case 5:
+                        build.append("пятьдесят");
+                        break;
+                    case 6:
+                        build.append("шестьдесят");
+                        break;
+                    case 7:
+                        build.append("семьдесят");
+                        break;
+                    case 8:
+                        build.append("восемьдесят");
+                        break;
+                    case 9:
+                        build.append("девяносто");
+                        break;
+                }
+
+                build.append(" ");
+                number %= 10;
+            }
+        }
+
+        switch (number) {
+            case 1:
+                build.append("один");
+                break;
+            case 2:
+                build.append("два");
+                break;
+            case 3:
+                build.append("три");
+                break;
+            case 4:
+                build.append("четыре");
+                break;
+            case 5:
+                build.append("пять");
+                break;
+            case 6:
+                build.append("шесть");
+                break;
+            case 7:
+                build.append("семь");
+                break;
+            case 8:
+                build.append("восемь");
+                break;
+            case 9:
+                build.append("девять");
+                break;
+        }
+
+        return build.toString();
+    }
+
+    // (8/10)
+    public static void getSha256Hash(String str) {
 
     }
 }
